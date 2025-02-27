629d75450ba1084a2316832aca87fbc37734eabd
Tian <kreenkld@gmail.com>
2025-02-27 14:49:24 +0100

    debut pass

---
+++ sources/commands/PASS.cpp
@@ -0,0 +1,13 @@
+
+#include "includes.hpp"
+#include "Server.hpp"
+
+void Server::pass(Message message, Client &client){
+    std::cout << "PASS command recieved" << std::endl;
+
+    if (message.params[0] == _password)
+	
+
+    return;
+}
+
