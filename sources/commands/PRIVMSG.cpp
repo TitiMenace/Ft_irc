0c805436212bac9b8f75a84ce2d1459910ce62df
Tian <kreenkld@gmail.com>
2025-02-28 11:25:45 +0100

    debut privmsg avec focntion pour envoyer a tous les clients d'un channel

--- sources/commands/PRIVMSG.cpp
+++ sources/commands/PRIVMSG.cpp
@@ -5,6 +5,16 @@
 // - ERR_NORECIPIENT (411)
 // - ERR_NOTEXTTOSEND (412)
 // - ERR_NOTREGISTERED (451)
+
+void	channelMessage(Channel channel, std::string output){
+	
+	for (std::map<int, Client>::iterator it = list_user.begin(); it != list_user.end(); it++){
+		
+	}
+	
+}
+
+
 void Server::privmsg(Message message, Client &client) {
 	const char *nickname = client.nickname.empty() ? "*" : client.nickname.c_str();
 
@@ -21,12 +31,18 @@ void Server::privmsg(Message message, Client &client) {
 		return;
 	}
 
-	std::string &target = message.params[1];
+	std::string &target = message.params[0];
 	std::string &text = message.params[1];
 	for (std::map<int, Client>::iterator it = _users.begin(); it != _users.end(); it++) {
 		if (it->first != client.socket_fd) {
 			dprintf(it->first, ":localhost PRIVMSG %s %s\r\n", target.c_str(), text.c_str());
 		}
 	}
+	std::map<std::string, Channel>::iterator it = _channel_list(message.params[0]);
+	if (it != _channel_list.end()){
+		channelMessage(*it, text);
+	}
+
+	
 }
 
