eeacbfccfa7940708df5036ca13ab790ac6bc16a
Tian <kreenkld@gmail.com>
2025-03-01 11:21:46 +0100

    join done et le rpl reply sont marchent dans irssi

--- includes/includes.hpp
+++ includes/includes.hpp
@@ -6,7 +6,7 @@
 #include <cstring> //memset
 #include <exception>
 #include <stdio.h> //perror
-
+#include <sstream>
 
 #include <netdb.h> 
 #include <netinet/in.h> 
--- sources/commands/JOIN.cpp
+++ sources/commands/JOIN.cpp
@@ -17,6 +17,60 @@ bool parseChannelID(std::string){
     return true;
 }
 
+void    joinMessage(Client &client, std::string channel_name){
+
+    std::string cname = channel_name;//.substr(1, channel_name.size());
+    dprintf(client.socket_fd, ":%s!%s@%s JOIN %s\r\n",client.nickname.c_str(),client.username.c_str(),client.hostname.c_str(), cname.c_str());
+    return;
+}
+
+void    RPL_ENDOFNAMES(Client &client, Channel &channel){
+
+    dprintf(2, "------------------DEBUG-------------------\n----------------channelname : %s---------------\n", channel.name.c_str());
+    std::stringstream	soutput;
+    std::string cname = channel.name;//.substr(1, channel.name.size());
+    std::string output;
+	//soutput << ":localhost ";
+
+	soutput << "366 " << client.nickname;
+	//soutput << " 353";
+
+	soutput << " " << cname <<  " :End of /NAMES list"; 
+    output = soutput.str();
+    output += "\r\n";
+     dprintf(2, "%s", output.c_str());
+    dprintf(client.socket_fd, "%s", output.c_str());
+	return;
+} 
+
+void    RPLNAMREPLY(Client &client, Channel &channel){
+
+
+    std::stringstream	soutput;
+
+    std::string cname = channel.name;//.substr(1, channel.name.size());
+    std::string output;
+	//soutput << ":localhost ";
+
+	soutput << "353 " << client.nickname;
+	//soutput << " 353";
+
+	soutput << " = " << cname <<  " : ";
+    for (std::map<int, Client>::iterator it = channel.list_user.begin(); it != channel.list_user.end(); it++){
+        if (channel.list_operator.find(it->first) != channel.list_operator.end()){
+            soutput << "@";
+        }
+	    soutput << it->second.nickname << " ";
+    }
+    output = soutput.str();
+    output.erase(output.size() - 1);
+    output += "\r\n";
+    std::cout << "je suis un caca dani caca le client socket fd est" << client.socket_fd << std::endl;
+    dprintf(2, "%s", output.c_str());
+    dprintf(client.socket_fd, "%s", output.c_str());
+	return;
+}
+
 void Server::join(Message message, Client &client){
     
     if (client.state != REGISTERED){
@@ -38,11 +92,14 @@ void Server::join(Message message, Client &client){
     if (it == _channel_list.end()) {
     	
 
-	_channel_list[channel_name] = Channel(channel_name, "topic", "password", 1);
+	_channel_list[channel_name] = Channel(channel_name, "topic", "password", 2);
 	_channel_list[channel_name].list_user[client.socket_fd] = client;
 	_channel_list[channel_name].list_operator[client.socket_fd] = client;
 	dprintf(2, "Channel : %s a bien ete cree et %s est bien user et %s est bien operateur\r\n", _channel_list[channel_name].name.c_str(), _channel_list[channel_name].list_user[client.socket_fd].nickname.c_str(), _channel_list[channel_name].list_operator[client.socket_fd].nickname.c_str());
-	return;
+        joinMessage(client, channel_name);
+        RPLNAMREPLY(client, _channel_list[channel_name]);
+        RPL_ENDOFNAMES(client, _channel_list[channel_name]);
+        return;
     }
 
     else if (_channel_list[channel_name].list_user.size() >= _channel_list[channel_name].size_limit){
@@ -52,6 +109,10 @@ void Server::join(Message message, Client &client){
 	
 	    it->second.list_user[client.socket_fd] = client;
 	    dprintf(2, "%s : est bien arrive dans le channel : %s\r\n", client.nickname.c_str(), _channel_list[channel_name].name.c_str());
+        joinMessage(client, channel_name);
+        RPLNAMREPLY(client, _channel_list[channel_name]);
+        RPL_ENDOFNAMES(client, _channel_list[channel_name]);
+
 	// if (_channel_list.find(message.params[0]) =! it.end)
     // Channel to_join = _channel_list[message.params[0]];
 
