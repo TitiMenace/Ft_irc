f733bb20d96c68063e2256db173a1ce9e19aab52
DaadfroGG <d.kermia@yahoo.com>
2025-03-10 21:46:25 +0100

    removed err msg from channel

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -36,26 +36,3 @@ struct Channel{
 std::string getchannelmodes(Channel channel);
 std::string getchannelmodarg(Channel channel);
 
-
-
-void    ERR_CHANNELISFULL(Client &client, Channel &channel);
-void    ERR_INVITEONLYCHAN(Client &client, Channel &channel);
-void    ERR_BADCHANNELKEY(Client &client, Channel &channel);
-void    ERR_UMODEUNKNOWNFLAG(Client &client, char c);
-
-void    ERR_NOSUCHCHANNEL(Client &client, std::string channel);
-void    ERR_CHANOPRIVSNEEDED(Client &client, std::string channel);
-    
-void    ERR_NOTONCHANNEL(Client &client, std::string channel);
-void    ERR_NOSUCHNICK(Client &client, std::string nickname);
-void    ERR_USERONCHANNEL(Client &client, std::string channel, std::string nick);
-
-void    ERR_NEEDMOREPARAMS(Client &client);
-void    ERR_USERSDONTMATCH(Client &client);
-void    ERR_ALREADYREGISTERED(Client &client);
-void    ERR_NORECIPIENT(Client &client, std::string nickname, Message &message);
-
-void    ERR_NOTEXTTOSEND(Client &client, std::string nickname);
-void    ERR_NOTREGISTERED(Client &client);
-
-void    ERR_BADCHANMASK(Client &client, std::string channel_name);
