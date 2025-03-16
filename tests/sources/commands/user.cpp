b0117ba0bc0bd344d5268da131bcd2c0e7c2d43c
Vélimir CHAKHNOVSKI <vchakhno@student.42.fr>
2025-03-16 22:51:02 +0100

    Tests (style): changed user tests to use ServerProcess/Client explicitely

--- tests/sources/commands/user.cpp
+++ tests/sources/commands/user.cpp
@@ -1,37 +1,39 @@
 #include <criterion/criterion.h>
 #include "tests.hpp"
 
+Test(user, not_enough_params) try {
+	ServerProcess server("password");
+	Client client(server.getPort());
 
-Test(user, registration) try {
-	std::string request = \
+	client.send(
 		"PASS password\r\n"
 		"NICK velimir\r\n"
-		"USER velimir * 0 velimir\r\n"
-	;
-	std::string expected_response = \
-		"001 velimir :Welcome to the WiZ insane chat of distortion of reality between worlds, velimir!velimir@velimir\r\n"
-		"002 velimir :Your host is , running version v.1\r\n"
-		"003 velimir :This server was created le 01/01/01\r\n"
-		"004 velimir :v.1 no user mode support +tlkoiq\r\n"
-	;
-
-	test_response(request, expected_response, "password");
+		"USER\r\n"
+	);
+	wait(1);
+	client.expectResponse(
+		"461 velimir USER :Not enough parameters\r\n"
+	);
 } catch (std::runtime_error e) {
 	cr_assert(false, "Error: %s", e.what());
 }
 
-Test(user, not_enough_params) try {
-	std::string request = \
+Test(user, registration) try {
+	ServerProcess server("password");
+	Client client(server.getPort());
+
+	client.send(
 		"PASS password\r\n"
 		"NICK velimir\r\n"
-		"USER\r\n"
-	;
-
-	std::string expected_response = \
-		"461 velimir USER :Not enough parameters\r\n";
-	;
-
-	test_response(request, expected_response, "password");
+		"USER velimir * 0 velimir\r\n"
+	);
+	wait(1);
+	client.expectResponse(
+		"001 velimir :Welcome to the WiZ insane chat of distortion of reality between worlds, velimir!velimir@velimir\r\n"
+		"002 velimir :Your host is , running version v.1\r\n"
+		"003 velimir :This server was created le 01/01/01\r\n"
+		"004 velimir :v.1 no user mode support +tlkoiq\r\n"
+	);
 } catch (std::runtime_error e) {
 	cr_assert(false, "Error: %s", e.what());
 }
