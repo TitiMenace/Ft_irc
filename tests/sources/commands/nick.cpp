394c03f19ad53dde18e90139c67eb3bb86164b90
Vélimir CHAKHNOVSKI <vchakhno@student.42.fr>
2025-03-16 22:39:49 +0100

    Tests NICK: added test for nickname_in_use

--- tests/sources/commands/nick.cpp
+++ tests/sources/commands/nick.cpp
@@ -2,21 +2,17 @@
 #include "tests.hpp"
 
 
-Test(nick, registration) try {
+Test(nick, not_enough_params) try {
 	ServerProcess server("password");
 	Client client(server.getPort());
 
 	client.send(
 		"PASS password\r\n"
-		"USER velimir * 0 velimir\r\n"
-		"NICK velimir\r\n"
+		"NICK\r\n"
 	);
 	wait(1);
 	client.expectResponse(
-		"001 velimir :Welcome to the WiZ insane chat of distortion of reality between worlds, velimir!velimir@velimir\r\n"
-		"002 velimir :Your host is , running version v.1\r\n"
-		"003 velimir :This server was created le 01/01/01\r\n"
-		"004 velimir :v.1 no user mode support +tlkoiq\r\n"
+		"461 :No nickname given\r\n"
 	);
 } catch (std::runtime_error e) {
 	cr_assert(false, "Error: %s", e.what());
@@ -40,17 +36,44 @@ Test(nick, erroneous_nickname) try {
 }
 
 
-Test(nick, not_enough_params) try {
+Test(nick, nickname_in_use) try {
+	ServerProcess server("password");
+	Client first(server.getPort());
+	Client second(server.getPort());
+
+	first.send(
+		"PASS password\r\n"
+		"NICK first\r\n"
+	);
+	wait(0.1);
+	second.send(
+		"PASS password\r\n"
+		"NICK first\r\n"
+	);
+	wait(1);
+	second.expectResponse(
+		"433 first :Nickname is already in use.\r\n"
+	);
+} catch (std::runtime_error e) {
+	cr_assert(false, "Error: %s", e.what());
+}
+
+
+Test(nick, registration) try {
 	ServerProcess server("password");
 	Client client(server.getPort());
 
 	client.send(
 		"PASS password\r\n"
-		"NICK\r\n"
+		"USER velimir * 0 velimir\r\n"
+		"NICK velimir\r\n"
 	);
 	wait(1);
 	client.expectResponse(
-		"461 :No nickname given\r\n"
+		"001 velimir :Welcome to the WiZ insane chat of distortion of reality between worlds, velimir!velimir@velimir\r\n"
+		"002 velimir :Your host is , running version v.1\r\n"
+		"003 velimir :This server was created le 01/01/01\r\n"
+		"004 velimir :v.1 no user mode support +tlkoiq\r\n"
 	);
 } catch (std::runtime_error e) {
 	cr_assert(false, "Error: %s", e.what());
