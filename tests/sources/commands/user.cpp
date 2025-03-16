df7b3b398fc6a7c5c5844e59b27ac6f8d4fb0320
Vélimir CHAKHNOVSKI <vchakhno@student.42.fr>
2025-03-16 22:57:37 +0100

    Tests USER: added test for already_registered

--- tests/sources/commands/user.cpp
+++ tests/sources/commands/user.cpp
@@ -37,3 +37,28 @@ Test(user, registration) try {
 } catch (std::runtime_error e) {
 	cr_assert(false, "Error: %s", e.what());
 }
+
+Test(user, already_registered) try {
+	ServerProcess server("password");
+	Client client(server.getPort());
+
+	client.send(
+		"PASS password\r\n"
+		"NICK velimir\r\n"
+		"USER velimir * 0 velimir\r\n"
+	);
+	wait(1);
+	client.expectResponse(
+		"001 velimir :Welcome to the WiZ insane chat of distortion of reality between worlds, velimir!velimir@velimir\r\n"
+		"002 velimir :Your host is , running version v.1\r\n"
+		"003 velimir :This server was created le 01/01/01\r\n"
+		"004 velimir :v.1 no user mode support +tlkoiq\r\n"
+	);
+
+	client.send("USER velimir * 0 velimir\r\n");
+	wait(1);
+	client.expectResponse("462 velimir :You may not reregister\r\n");
+
+} catch (std::runtime_error e) {
+	cr_assert(false, "Error: %s", e.what());
+}
