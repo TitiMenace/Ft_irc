18da0f10bbbc5cf5ccc25b5c8e288f08c4ab587d
Velimir CHAKHNOVSKI <vchakhno@student.42.fr>
2025-03-17 15:29:00 +0100

    Fix: removed extra space in PONG reply

--- sources/commands/PING_PONG.cpp
+++ sources/commands/PING_PONG.cpp
@@ -6,7 +6,7 @@ void Server::ping(Message message, Client &client){
     std::string token = "";
     if (message.params.size() >= 1)
     	token = token + message.params[0];
-    std::string output = "PONG : " + token;
+    std::string output = "PONG :" + token;
     output += "\r\n";
     client.outBuffer += output;
     return;
