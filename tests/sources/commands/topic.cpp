b62731d20025f7a85f9cc56c99557415cdfa561e
Velimir CHAKHNOVSKI <vchakhno@student.42.fr>
2025-04-01 18:26:17 +0200

    Test (TOPIC): added test for protected topic

--- tests/sources/commands/topic.cpp
+++ tests/sources/commands/topic.cpp
@@ -130,3 +130,44 @@ Test(topic, valid_set_topic) try {
 	cr_assert(false, "Error: %s", e.what());
 }
 
+
+Test(topic, protected_topic) try {
+	ServerProcess server("password");
+	Client first(server.getPort());
+	Client second(server.getPort());
+
+	first.register_("password", "first");
+	second.register_("password", "second");
+
+	first.send("JOIN #channel\r\n");
+	wait(0.1);
+	first.expectResponse(
+		":first!first@ JOIN #channel\r\n"
+		"353 first = #channel :@first\r\n"
+		"366 first #channel :End of /NAMES list\r\n"
+	);
+
+	second.send("JOIN #channel\r\n");
+	wait(0.1);
+	first.expectResponse(":second!second@ JOIN #channel\r\n");
+	second.expectResponse(
+		":second!second@ JOIN #channel\r\n"
+		"353 second = #channel :@first second\r\n"
+		"366 second #channel :End of /NAMES list\r\n"
+	);
+
+	first.send("MODE #channel +t\r\n");
+	wait(0.1);
+	first.expectResponse(":first MODE #channel +t\r\n");
+	second.expectResponse(":first MODE #channel +t\r\n");
+
+	second.send("TOPIC #channel topic\r\n");
+	wait(0.1);
+	second.expectResponse("482 second #channel :You're not channel operator\r\n");
+
+	first.send("TOPIC #channel topic\r\n");
+	wait(0.1);
+	first.expectResponse(":first TOPIC #channel :topic\r\n");
+} catch (std::runtime_error e) {
+	cr_assert(false, "Error: %s", e.what());
+}
